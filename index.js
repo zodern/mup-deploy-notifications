2b30d87a0c33d654e530368ee3f6b4680c36a69b
zodern <zodern@users.noreply.github.com>
2020-07-29 17:11:34 -0500

    Add context with hostname and git commit

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const { execSync } = require('child_process');
+const { execSync, execFileSync } = require('child_process');
 const joi = require('joi');
+const os = require('os');
+const fs = require('fs');
 
-function sendMessage(status, message, endpoint, channel) {
-  const output = execSync(`node ${__dirname}/send-message.js ${status} "${message}" "${endpoint}" "${channel}"`)
-  console.log(output.toString('utf-8'))
+// We send the message while the process is exiting, so we can only do synchronous work.
+// To work around that, we send the message in a child process using execSync
+function sendMessage({
+  status,
+  message,
+  fallback,
+  context,
+  endpoint,
+  channel
+}) {
+  const output = execSync(`node ${__dirname}/send-message.js ${status} "${message}" "${fallback}" "${context}" "${endpoint}" "${channel}"`);
+  console.log(output.toString('utf-8'));
 }
 
 
@@ -31,30 +42,58 @@ module.exports = {
         app,
         deployNotifications
       } = api.getConfig();
+
       if (!deployNotifications) {
-        return
+        return;
       }
 
-      const appName = app.name
-      const appUrl = app.env && app.env.ROOT_URL ? app.env.ROOT_URL : ''
+      const appName = app.name;
+      const appPath = api.resolvePath(api.getBasePath(), app.path);
+      let commitHash = '<unable to retrieve git commit>';
+      let commitMessage = '';
+
+      if (fs.existsSync(api.resolvePath(appPath, '.git'))) {
+        try {
+          const commitHashOutput = execFileSync("git", ["rev-parse", "--short", "HEAD"], {
+            cwd: appPath,
+          });
+          commitHash = commitHashOutput.toString('utf-8').trim();
+
+          const messageOutput = execFileSync("git", ["show", "-s", "--format=%s", commitHash], {
+            cwd: appPath,
+          });
+          commitMessage = `(${messageOutput.toString('utf-8').trim()})`;
+        } catch (e) {
+          console.error('Unable to retrieve git commit details');
+          console.error(e);
+        }
+      }
+
+      const appUrl = app.env && app.env.ROOT_URL ? app.env.ROOT_URL : '';
+      const isCI = process.env.CI === 'true';
+      const context = `Deployed commit *${commitHash}* ${commitMessage} from *${isCI ? 'CI' : os.hostname()}*`;
 
       process.on('exit', async (code) => {
         if (code > 0) {
-          sendMessage(
-            'danger',
-            `Deploy for <${appUrl}|${appName}> failed.`,
-            deployNotifications.slackWebhookUrl,
-            deployNotifications.slackChannel
-          )
+          sendMessage({
+            status: 'danger',
+            message: `Deploy for <${appUrl}|${appName}> failed.`,
+            fallback: `Deploy for ${appName} failed.`,
+            context,
+            endpoint: deployNotifications.slackWebhookUrl,
+            channel: deployNotifications.slackChannel
+          });
         } else {
-          sendMessage(
-            'good',
-            `Deploy for <${appUrl}|${appName}> succeeded.`,
-            deployNotifications.slackWebhookUrl,
-            deployNotifications.slackChannel
-          )
+          sendMessage({
+            status: 'good',
+            message: `Deploy for <${appUrl}|${appName}> succeeded.`,
+            fallback: `Deploy for ${appName} succeeded.`,
+            context,
+            endpoint: deployNotifications.slackWebhookUrl,
+            channel: deployNotifications.slackChannel
+          });
         }
-      })
+      });
     }
   }
-}
+};
--- send-message.js
+++ send-message.js
@@ -1,20 +1,41 @@
 const axios = require('axios');
 
-const color = process.argv[2]
-const message = process.argv[3]
-const endpoint = process.argv[4]
-const channel = process.argv[5]
+const [
+  status,
+  message,
+  fallback,
+  context,
+  endpoint,
+  channel
+] = process.argv.slice(2);
 
 async function send() {
   axios.post(endpoint, {
-    "channel": channel,
-    "text": "",
+    channel: channel,
     attachments: [{
-      color: color,
-      mrkdwn_in: 'text',
-      text: message
+      // When using blocks, slack only shows the color if it is a hex code
+      color: status === 'danger' ? '#D63232' : '#36A64F',
+      fallback: fallback,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: message,
+          }
+        },
+        {
+          type: 'context',
+          elements: [
+            {
+              type: 'mrkdwn',
+              text: context
+            }
+          ]
+        }
+      ]
     }]
-  })
+  });
 }
 
-send()
+send();
